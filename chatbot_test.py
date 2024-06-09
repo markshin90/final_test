ae67c415f3b9d36005cf581612149456f38c3af3
markshin90 <166914288+markshin90@users.noreply.github.com>
2024-06-09 15:15:59 +0900

    Add files via upload

---
+++ chatbot_test.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import Levenshtein
+
+class SimpleChatBot:
+    def __init__(self, filepath):
+        self.questions, self.answers = self.load_data(filepath)
+
+    def load_data(self, filepath):
+        data = pd.read_csv(filepath)
+        questions = data['Q'].tolist()  # 질문열만 뽑아 파이썬 리스트로 저장
+        answers = data['A'].tolist()   # 답변열만 뽑아 파이썬 리스트로 저장
+        return questions, answers
+
+    def find_best_answer(self, input_sentence):
+        distances = [Levenshtein.distance(input_sentence, question) for question in self.questions] # 입력된 질문과 load_data 질문열의 레벤슈타인 거리 구하기
+        best_match_index = distances.index(min(distances))  # 레벤슈타인 거리가 가장 작은 값의 인덱스를 반환
+        return self.answers[best_match_index]
+
+# CSV 파일 경로를 지정하세요.
+filepath = 'ChatbotData.csv'
+
+# 간단한 챗봇 인스턴스를 생성합니다.
+chatbot = SimpleChatBot(filepath)
+
+# '종료'라는 단어가 입력될 때까지 챗봇과의 대화를 반복합니다.
+while True:
+    input_sentence = input('You: ')
+    if input_sentence.lower() == '종료':
+        break
+    response = chatbot.find_best_answer(input_sentence)
+    print('Chatbot:', response)
